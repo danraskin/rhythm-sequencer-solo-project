17f2de6935a93ee8d8ca59ec491738df0cd3a2c5
Dan Raskin <Danieltraskin@gmail.com>
2022-10-13 11:42:47 -0500

    seeded drum_kits table

--- database.sql
+++ database.sql
@@ -1,8 +1,3 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
@@ -11,43 +6,50 @@ CREATE TABLE "user" (
     "updated_at" TIMESTAMP
 );
 
-CREATE TABLE "patterns" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER FOREIGN KEY "user",
-    "kit_id" INTEGER FOREIGN KEY "drum_kits",
-    "name" VARCHAR (80) NOT NULL,
-    "steps" INTEGER NOT NULL,
-    "steps_total" INTEGER NOT NULL,
-    "inserted_at" TIMESTAMP,
-    "updated_at" TIMESTAMP
-)
-
 CREATE TABLE "drum_kits" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (50),
     "BD" VARCHAR (500),
     "SD" VARCHAR (500),
     "HH" VARCHAR (500)
-)
+);
+
+CREATE TABLE "patterns" (
+    "id" SERIAL PRIMARY KEY,
+    -- "user_id" INT REFERENCES "user",
+    "kit_id" INT REFERENCES "drum_kits" NOT NULL,
+    "name" VARCHAR (80) NOT NULL,
+    "steps" INT NOT NULL,
+    "steps_total" INT NOT NULL,
+    "inserted_at" TIMESTAMP,
+    "updated_at" TIMESTAMP
+);
 
 CREATE TABLE "drum_type_lookup" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (2)
-)
+);
 
 CREATE TABLE "steps" (
     "id" SERIAL PRIMARY KEY,
-    "pattern_id" INTEGER FOREIGN KEY "patterns",
-    "step" INTEGER,
+    "pattern_id" INT REFERENCES "patterns" NOT NULL,
+    "step" INT,
     "BD" BOOLEAN,
     "SD" BOOLEAN,
     "HH" BOOLEAN
-)
+);
 
 CREATE TABLE "pattern_params" (
     "id" SERIAL PRIMARY KEY,
-    "pattern_id" INTEGER FOREIGN KEY "patterns",
-    "drum_id" INTEGER FOREIGN KEY "drum_type_lookup",
-    "param_volume" INTEGER,
-    "param_pitch" INTEGER
-)
+    "pattern_id" INT REFERENCES "patterns" NOT NULL,
+    "drum_id" INT REFERENCES "drum_type_lookup" NOT NULL,
+    "param_volume" INT,
+    "param_pitch" INT
+);
+
+-- SEED DRUM_KITS
+
+INSERT INTO "drum_kits" ("name","BD","SD","HH")
+    VALUES
+    ('Test kit 1','BD.WAV','TOTAL_808_SAMPLE 9_S08.WAV','TOTAL_808_SAMPLE 11_S10.WAV'),
+    ('Test kit 2','TOTAL_808_SAMPLE 26_S25.WAV','TOTAL_808_SAMPLE 17_S16.WAV','TOTAL_808_SAMPLE 23_S22.WAV');
